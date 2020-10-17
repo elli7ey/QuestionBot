10e8568646959eb424d1cad1756e907f25f9aa05
elli7ey <smirn.elysees@gmail.com>
2020-10-17 20:25:10 +0300

    Some updates

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ command = ["history_add"]
 @ebot.message_handler(commands=command)
 @ebot.edited_message_handler(commands=command)
 def history_add(message):
-    ebot.reply_to(message, "Вы хотите добавить вопрос по истории. Для этого отправь мне вопрос, который ты хочешь добавить в базу данных")
+    ebot.reply_to(message, "Вы хотите добавить вопрос/вопросы по истории. Для этого отправьте все одним сообщением. Сообщение должно быть отформатированно так:\n\nВопрос[enter]\n[enter]\nОвтет[enter]\n[enter]\n[enter]\nВопрос[enter]\n[enter]\nОтвет[enter]\n[enter]\n[enter]\n\nИ так далее. \nНе обессудь брат братишка просто надо как-то разделить вопрос и ответ")
     msgs = fetch(MESSAGE_PATH)
     msgs[message.from_user.id] = ["Ожидание вопроса", "История"]
     store(MESSAGE_PATH, msgs)
@@ -85,7 +85,7 @@ command = ["social_add"]
 @ebot.message_handler(commands=command)
 @ebot.edited_message_handler(commands=command)
 def history_add(message):
-    ebot.reply_to(message, "Вы хотите добавить вопрос по обществознанию. Для этого отправь мне вопрос, который ты хочешь добавить в базу данных. Отступи три строчки и напиши ответ на этот вопрос. Не обессудь брат братишка, мне просто надо как-то разделить вопрос и ответ")
+    ebot.reply_to(message, "Вы хотите добавить вопрос/вопросы по обществознанию. Для этого отправьте все одним сообщением. Сообщение должно быть отформатированно так:\n\nВопрос[enter]\n[enter]\nОвтет[enter]\n[enter]\n[enter]\nВопрос[enter]\n[enter]\nОтвет[enter]\n[enter]\n[enter]\n\nИ так далее. \nНе обессудь брат братишка просто надо как-то разделить вопрос и ответ")
     msgs = fetch(MESSAGE_PATH)
     msgs[message.from_user.id] = ["Ожидание вопроса", "Общество"]
     store(MESSAGE_PATH, msgs)
@@ -94,24 +94,47 @@ command = ["history_generate"]
 @ebot.message_handler(commands=command)
 @ebot.edited_message_handler(commands=command)
 def history_generate(message):
+    msgs = fetch(MESSAGE_PATH)
+    cid = message.chat.id
     res = ""
+    valid_questions = []
+
     numbers = generate_numbers(20, HISTORY_PATH)
     questions = get_questions(HISTORY_PATH, numbers)
-    for row in questions:
-        res += f"{row[0]}\n{row[1]}\n\n"
+
+    for id, row in enumerate(questions):
+        res += f"{id+1}. {row[0]}\n{row[1]}\n\n"
+        valid_questions.append([row[0], row[1]])
+
     ebot.reply_to(message, res)
+
+    ebot.send_message(cid, "Если вы хотите изменить какой-то вопрос, то отправьте мне его номер по счету. За раз, вы сможете изменить только один вопрос.")
+    msgs[message.from_user.id] = ["Возможна замена", valid_questions]
+    store(MESSAGE_PATH, msgs)
 
 
 command = ["social_generate"]
 @ebot.message_handler(commands=command)
 @ebot.edited_message_handler(commands=command)
-def history_generate(message):
+def social_generate(message):
+    msgs = fetch(MESSAGE_PATH)
+    cid = message.chat.id
     res = ""
+    valid_questions = []
+
     numbers = generate_numbers(20, SOCIAL_PATH)
     questions = get_questions(SOCIAL_PATH, numbers)
-    for row in questions:
-        res += f"{row[0]}\n{row[1]}\n\n"
+
+    for id, row in enumerate(questions):
+        res += f"{id+1}. {row[0]}\n{row[1]}\n\n"
+        valid_questions.append([row[0], row[1]])
+
     ebot.reply_to(message, res)
+
+    ebot.send_message(cid, "Если вы хотите изменить какой-то вопрос, то отправьте мне его номер по счету. За раз, вы сможете изменить только один вопрос.")
+    msgs[message.from_user.id] = ["Возможна замена", valid_questions]
+    store(MESSAGE_PATH, msgs)
+
 
 
 
@@ -119,6 +142,7 @@ def history_generate(message):
 @ebot.message_handler(func=lambda m: True)
 def echo_all(message):
     msgs = fetch(MESSAGE_PATH)
+    cid = message.chat.id
     last_msg = msgs[message.from_user.id]
 
     if last_msg[0] == "Ожидание вопроса":
@@ -126,29 +150,57 @@ def echo_all(message):
         msgs[message.from_user.id] = [message.text, last_msg[1]]
         store(MESSAGE_PATH, msgs)
 
-    if message.text == "Да":
-        if "\n\n\n" in last_msg[0]:
-            f_column, s_column = last_msg[0].split("\n\n\n")
+    elif message.text == "Да":
+        if "\n\n\n" in last_msg[0] or "\n\n" in last_msg[0]:
+            questions = last_msg[0].split("\n\n\n")
+            x = 0
+            valid_questions = []
+            for question in questions:
+                print(x, question)
+                if question.count("\n\n") == 1:
+                    f_column, s_column = question.split("\n\n")
+                    x += 1
 
-            if last_msg[1] == "Общество":
-                if add_question(SOCIAL_PATH, f"*{f_column}*", f"({s_column})"):
-                    ebot.reply_to(message, "Готово")
+                    if last_msg[1] == "Общество":
+                        q_path = SOCIAL_PATH
+                    elif last_msg[1] == "История":
+                        q_path = HISTORY_PATH
+
+                    if add_question(q_path, f"*{f_column}*", f"({s_column})"):
+                        valid_questions.append([f_column, s_column])
+                        ebot.send_message(cid, f"Вопрос номер {x} - успешно добавлен")
+                    else:
+                        ebot.send_message(cid, f"{x} - вопрос уже присутвует в базе данных")
+
                 else:
-                    ebot.reply_to(message, "Данный вопрос уже присутвует в базе данных")
+                    ebot.send_message(cid, f"Ты не правильно отформатировал сообщение под номером {x}")
 
-            if last_msg[1] == "История":
-                if add_question(HISTORY_PATH, f"*{f_column}*", f"({s_column})"):
-                    ebot.reply_to(message, "Готово")
-                else:
-                    ebot.reply_to(message, "Данный вопрос уже присутвует в базе данных")
 
-            msgs[message.from_user.id] = "Ждемс запроса"
+            msgs[message.from_user.id] = ["Ждемс", ""]
             store(MESSAGE_PATH, msgs)
 
+    elif last_msg[0] == "Возможна замена" and message.text.isdigit():
+        num = int(message.text)
+        if num <= 20:
+            ebot.reply_to(message, "Отправь мне новый вариант вопроса.\n\nВопрос[enter]\n\nОтвет")
+            msgs[message.from_user.id] = ["Ожидание исправленного вопроса", (num, last_msg[1])]
         else:
-            ebot.reply_to(message, "Вы не правильно отформатировали свое сообщение")
-            msgs[message.from_user.id] = ["Ожидание вопроса", last_msg[1]]
-            store(MESSAGE_PATH, msgs)
+            ebot.reply_to(message, "Такого номера не существует. Отправь номер заново")
+
+    elif last_msg[0] == "Ожидание исправленного вопроса":
+        num, new_questions = last_msg[1]
+        new_f_c, new_s_c = message.text.split["\n\n"]
+        last_f_c, last_s_c = new_questions[-1]
+
+        new_questions[num-1] = [f"*{new_f_c}*", f"({new_s_c})"]
+        res = ""
+        for id, row in enumerate(new_questions):
+            res += f"{id+1}. {row[0]}\n{row[1]}\n\n"
+        ebot.send_message(cid, res)
+
+
+
+
 
 
 
